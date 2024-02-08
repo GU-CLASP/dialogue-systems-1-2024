8f789fb8064272ae8c6b4c35b73d9b248a2eedd8
anurni <anninieminen23@gmail.com>
2024-02-08 13:10:16 +0100

    implemented all speak and listen actions, does not work yet tho

--- Code/dm.js
+++ Code/dm.js
@@ -41,13 +41,23 @@ function getPerson(utterance) {
 
 const dmMachine = setup({
   actions: {
-    listenForUsersAnswer : ({ context }) => context.ssRef.send({ type: "LISTEN" }),
-    speakToTheUser : ({ context }) => context.ssRef.send({ type: "SPEAK"}),
-    assignMeetingName : assign({meeting_name: (context, event) => event.value[0].utterance})
+    listenForUsersAnswer : ({ context }) => 
+    context.ssRef.send({
+       type: "LISTEN" }),
+
+    speakToTheUser : ({ context }, params) => 
+    context.ssRef.send({
+       type: "SPEAK",
+      value: {
+        utterance: params
+      },
+    }),
+
+    //assignMeetingName : assign({meeting_name: ({context, event}) => event.value[0].utterance})
+
 }}).createMachine({
   /** @xstate-layout N4IgpgJg5mDOIC5QBECyA6ACgJzABwENcBiAQQGUAlAFWvIH1KBRU5ATQG0AGAXUVDwB7WAEsALiMEA7fiAAeiAIwBOAOwAaEAE8li1egCsAXyOa06AOoFx1QeTFExxAMIAZAJLOA0tz5IQQqIS0rIKCCpc6ABMBpo6CAYAHAAs0camIOY4ggC2eGKkUhCksADWWNi5+cTkmCxe9M4A8qiYrkzUTL6ygeKSMv5hyar6qrHaiABsk4roitMLiwsmZhjZeQVFJeXbxMzNAOIAcu7kTMjd-r3BA6Bh04mGkzFxiAYGiiuZa5UbhcVldC7Wr1RotNodLq8HrCPohQZKSbJV4IZKKZJfczIaRgFwebyXASwm6hRHIiYJADMlPQYxMGSkgggcFkaBhQX6pIQAFpJijeZifvgiGB2XDbvJEMkoijErN0qtLNYxLZ7I4xSSEQkNBSkqk6Rksr98v9thrOVqoqp9eN4slJjTkspncoopTVFxJlwDMNBRUqpsAeV1vlzfC7ohKVEorTbVNKY95ktk36Q4HtkCymGJfdJgZ0JSDMp3XGEHmaTE-dipKKrsSLRGEjKKdLIlxEtTO13qfSjEA */
   context: {
-    count: 0,
     meeting_name: '',
     meeting_date: '',
     meeting_time: '',
@@ -73,34 +83,25 @@ const dmMachine = setup({
     },
 
     Prompt: {
-      entry: ({ context }) =>
-        context.ssRef.send({
-        type: "SPEAK",
-        value: {
-          utterance: `Let's create an appointment!`,
-          },
-        }),
+      entry: [{ type: "speakToTheUser", 
+      params: `Let's create an appointment!`,
+          }],
       on: {
       SPEAK_COMPLETE : "MeetingPersonSpeak",
       },
     },
 
     MeetingPersonSpeak: {
-      entry: ({ context }) =>
-        context.ssRef.send({
-        type: "SPEAK",
-        value: {
-          utterance: `Who would you like to meet?`,
-            },
-          }),
-      on: { SPEAK_COMPLETE: "MeetingPersonListen" },
-        },
+      entry: [{ type: "speakToTheUser", 
+      params: `Who would you like to meet?`,
+          }],
+      on: { 
+        SPEAK_COMPLETE: "MeetingPersonListen"
+      },
+    },
 
     MeetingPersonListen: {
-      entry: ({ context }) =>
-      context.ssRef.send({
-      type: "LISTEN",
-      }),
+      entry: "listenForUsersAnswer",
       on: {
         RECOGNISED : { 
           actions: assign({meeting_name: ({context, event}) => event.value[0].utterance}),
@@ -110,21 +111,16 @@ const dmMachine = setup({
   },
 
     MeetingDaySpeak: {
-      entry: ({ context }) =>
-        context.ssRef.send({
-        type: "SPEAK",
-        value: {
-          utterance: `On which day is your meeting?`,
-              },
-            }),
-      on: { SPEAK_COMPLETE : "MeetingDayListen" }
-          },
+      entry: [{ type: "speakToTheUser", 
+          params: `On which day is your meeting?`,
+              }],
+      on: { 
+        SPEAK_COMPLETE : "MeetingDayListen"
+       },
+      },
 
     MeetingDayListen: {
-      entry: ({ context }) =>
-        context.ssRef.send({
-        type: "LISTEN",
-        }),
+      entry: "listenForUsersAnswer",
       on: {
         RECOGNISED : { 
             actions: assign({meeting_date: ({context, event}) => event.value[0].utterance}),
@@ -134,21 +130,15 @@ const dmMachine = setup({
         },
 
     MeetingDurSpeak: {
-      entry: ({ context }) =>
-        context.ssRef.send({
-        type: "SPEAK",
-        value: {
-          utterance: `Will it take the whole day?`,
-              },
-            }),
-      on: { SPEAK_COMPLETE: "MeetingDurListen" }
-          },
+      entry: [{ type: "speakToTheUser", 
+      params: `Will it take the whole day?`,
+          }],
+      on: { SPEAK_COMPLETE: "MeetingDurListen"
+     },
+    },
 
     MeetingDurListen: {
-      entry: ({ context }) =>
-        context.ssRef.send({
-        type: "LISTEN",
-          }),
+      entry: "listenForUsersAnswer",
       on: {
         RECOGNISED : { 
           actions: assign({meeting_dur: ({context, event}) => event.value[0].utterance}),
@@ -158,22 +148,16 @@ const dmMachine = setup({
     },
 
     MeetingTimeSpeak: {
-      entry: ({ context }) =>
-        context.ssRef.send({
-        type: "SPEAK",
-        value: {
-          utterance: `What time is your meeting?`,
-              },
-            }),
-      on: { SPEAK_COMPLETE: "MeetingTimeListen" },
-        },
+      entry: [{ type: "speakToTheUser", 
+      params: `What time is your meeting?`,
+          }],
+      on: { SPEAK_COMPLETE: "MeetingTimeListen" 
+    },
+  },
 
 
     MeetingTimeListen: {
-      entry: ({ context }) =>
-        context.ssRef.send({
-        type: "LISTEN",
-          }),
+      entry: "listenForUsersAnswer",
       on: {
         RECOGNISED : { 
           actions: assign({meeting_time: ({context, event}) => event.value[0].utterance}),
@@ -183,17 +167,13 @@ const dmMachine = setup({
       },
 
     Verification: {
-      entry: ({ context }) =>
-              context.ssRef.send({
-              type: "SPEAK",
-              value: {
-                utterance: `Do you want me to create an appointment with ${
-                      context.meeting_name} on ${context.meeting_date}
-                       at ${context.meeting_time}?`
-                  },
-                }),
-      on: { SPEAK_COMPLETE: "#DM.Done"}
+      entry: [{ type: "speakToTheUser", 
+      params: `Do you want me to create an appointment with ${context.meeting_name} on ${context.meeting_date} at ${context.meeting_time}?`,
+          }],
+      on: { SPEAK_COMPLETE: "#DM.Done" 
     },
+  },
+
     Done: {
       on: {
         CLICK: "Prompt",
